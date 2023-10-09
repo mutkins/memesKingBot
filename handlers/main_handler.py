f3b35a1e0e097cbe2e475fedc1e2f4f4fb3acfd1
Mikhail Utkin <mihut@rambler.ru>
2023-10-09 09:34:21 +0500

    fix delete caption error

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -20,13 +20,13 @@ async def collect_msgs(message: types.Message):
 
     match message.content_type:
         case 'photo':
-            await message.answer_photo(photo=file_id, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
+            await message.answer_photo(photo=file_id, caption=message.caption, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
         case 'video':
             await message.answer_video(video=file_id, caption=message.caption, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
         case 'animation':
-            await message.answer_animation(animation=file_id, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
+            await message.answer_animation(animation=file_id, caption=message.caption, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
         case 'document':
-            await message.answer_document(document=file_id, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
+            await message.answer_document(document=file_id, caption=message.caption, reply_markup=get_likes_kb(db_msg_id=db_msg_id))
     await message.delete()
 
 
