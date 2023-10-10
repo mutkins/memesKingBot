f05f9897d0c543f20eeb1696104cd073e146b017
Mikhail Utkin <mihut@rambler.ru>
2023-10-10 13:52:46 +0500

    add get log by admin

--- handlers/main.py
+++ handlers/main.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 from handlers.main_handler import collect_msgs, set_like
+from handlers.admin_actions import get_log
 
 
 def register_all_handlers(dp: Dispatcher):
+    dp.register_message_handler(get_log, state='*', commands=['log'])
     dp.register_message_handler(collect_msgs, content_types=['photo', 'document', 'animation', 'video'])
     dp.register_callback_query_handler(set_like, lambda callback: callback.data.startswith('like'))
