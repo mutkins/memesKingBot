590c235ff7bd7a7c7de7ce0cf90cfd5eb7c8670f
mutkins <113161484+mutkins@users.noreply.github.com>
2024-05-21 12:12:03 +0500

    Update db_init.py

--- db/db_init.py
+++ db/db_init.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # Declarative method
-engine = create_engine("sqlite:////etc/tg_bots/data/king_of_memes.db", echo=True)
+engine = create_engine("sqlite:////etc/tg_bots_data/king_of_memes.db", echo=True)
 Base = declarative_base()
 
 
